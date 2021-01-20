9386186360cc7ee71a9dae84e4463c29ac228710
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-20 13:42:03 +0900

    WIP:

---
+++ todo_app_php_v4-master/work/public/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      checkbox.parentNode.submit();
+    });
+  });
+
+  const deletes = document.querySelectorAll('.delete');
+  deletes.forEach(span => {
+    span.addEventListener('click', () => {
+      if (!confirm('マジで消すの？')) {
+        return;
+      }
+      span.parentNode.submit();
+    });
+  });
+}

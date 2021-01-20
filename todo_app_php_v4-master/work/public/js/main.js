089bad52e40c64dabe58ccccf7d5ef3c97d21797
ken505 <68226398+ken505@users.noreply.github.com>
2021-01-20 21:42:59 +0900

    Done

--- todo_app_php_v4-master/work/public/js/main.js
+++ todo_app_php_v4-master/work/public/js/main.js
@@ -1,19 +1,27 @@
 "use strict";
 {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", () => {
       checkbox.parentNode.submit();
     });
   });
 
-  const deletes = document.querySelectorAll('.delete');
-  deletes.forEach(span => {
-    span.addEventListener('click', () => {
-      if (!confirm('マジで消すの？')) {
+  const deletes = document.querySelectorAll(".delete");
+  deletes.forEach((span) => {
+    span.addEventListener("click", () => {
+      if (!confirm("削除しますか？")) {
         return;
       }
       span.parentNode.submit();
     });
+  });
+
+  const purge = document.querySelector(".purge");
+  purge.addEventListener("click", () => {
+    if (!confirm("削除しますか？")) {
+      return;
+    }
+    purge.parentNode.submit();
   });
 }
